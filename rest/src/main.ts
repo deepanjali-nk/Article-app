7bb235cfa24e17dc3aac98837c2b0217902c45a8
deepanjali-nk <deepanjalinagarkoti@gmail.com>
2025-04-03 14:38:58 +0545

    feat: Error handling using exception filter

--- rest/src/main.ts
+++ rest/src/main.ts
@@ -1,20 +1,22 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({whitelist: true}));
 
   const config = new DocumentBuilder()
-    .setTitle('rest API')
-    .setDescription('The rest API description')
+    .setTitle('Median')
+    .setDescription('The Median API description')
     .setVersion('0.1')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
   await app.listen(3000);
 }
